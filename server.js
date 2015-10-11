57b8e9771a25ba85280d944280f1de906b58ed51
Adi Peleg <adifish21@gmail.com>
2015-10-11 12:36:32 +0300

    Add DELETE todos route

--- server.js
+++ server.js
@@ -48,6 +48,20 @@ app.post('/todos', function (req, res){
 })
 
 
+//DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var todoItem = _.findWhere(todos, {id: todoId});
+
+	if(!todoItem){
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, todoItem);
+		res.json(todoItem);
+	}
+
+});
+
 app.listen(PORT, function (){
 	console.log('Express listening on port ' + PORT + "!");
 });
