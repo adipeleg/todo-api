7060903321caf0c0f69f611d8de20aa118c4e55b
Adi Peleg <adifish21@gmail.com>
2015-10-11 15:53:56 +0300

    add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,33 @@
+// var person = {
+// 	name: 'Adi',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Adi',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+//Array example
+
+var array = [0,1,2,3,4];
+
+function pushIt (array){
+	array.push(5);
+	debbuger;
+}
+
+function newArray (array){
+	array = [0,1,2,3,4,5];
+}
+
+//pushIt(array);
+newArray(array);
+console.log(array);
